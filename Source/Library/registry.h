0c5b9c61d0b3ed6ebe80030f96ca0ac90faf3dae
Adrian Wenz <support@binary-space.com>
2017-08-31 23:00:02 +0200

    Add files via upload

--- Source/Library/registry.h
+++ Source/Library/registry.h
@@ -102,6 +102,8 @@ private:
 	BOOL SetSecurity(HKEY hKey, LPCTSTR pszSubKey, CONST CSecurityDescriptor &cSecurityDescriptor);
 	BOOL GetSecurity(HKEY hKey, LPCTSTR pszSubKey, CSecurityDescriptor &cSecurityDescriptor) CONST;
 
+	REGSAM CheckSAM(HKEY hKey, REGSAM regSAM) CONST;
+
 	CString ConstructQualifiedSubKey(LPCTSTR pszSubKey) CONST;
 };
 
